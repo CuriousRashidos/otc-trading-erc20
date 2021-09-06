9471aa576918403fece5ce0b91210734a25fdbb6
Rashid Jaffar <vergil1998@hotmail.com>
2021-09-06 13:04:15 +0400

    Delete credentials.js

--- src/credentials.js
+++
@@ -1,4 +0,0 @@
-export const BASE = "appcqBiLbAaAj5qTW";
-export const PRIVATE_KEY = "keyZAxdqXHhB3qrH0";
-export const METAMASK_TESTACCOUNT =
-  "9cc7db30c1647301c839ba930fbe97d12ed7788099910e1116d27d98cb65d4d7";
