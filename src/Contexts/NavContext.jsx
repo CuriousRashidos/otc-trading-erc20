6a166769fa5e55fb30e26db386e5c3eee1aa35ec
Rashid Jaffar <vergil1998@hotmail.com>
2021-09-06 13:03:12 +0400

    Update NavContext.jsx

--- src/Contexts/NavContext.jsx
+++ src/Contexts/NavContext.jsx
@@ -1,7 +1,7 @@
 import { createContext, useState } from "react";
 export const NavCtx = createContext(null);
 export const NavContext = ({ children }) => {
-  const [page, setPage] = useState("sell");
+  const [page, setPage] = useState("mint");
   return (
     <NavCtx.Provider value={{ page, setPage }}>{children}</NavCtx.Provider>
   );
